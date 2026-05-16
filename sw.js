69cfd8dc9ddf67f452ac1afe22d274eee5e61809
Teltenkoetter <52072451+Teltenkoetter@users.noreply.github.com>
2026-05-16 21:07:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'memofix-v99';
+const CACHE = 'memofix-v100';
 const SHELL = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
